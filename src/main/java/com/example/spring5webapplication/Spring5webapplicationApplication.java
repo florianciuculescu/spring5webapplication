ca0afa92716db1778b94f5ddc55a27f847102c97
fciuculescu <fciuculescu@deloittece.com>
2020-08-25 12:04:53 +0300

    first commit

---
+++ src/main/java/com/example/spring5webapplication/Spring5webapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring5webapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5webapplicationApplication.class, args);
+    }
+
+}
